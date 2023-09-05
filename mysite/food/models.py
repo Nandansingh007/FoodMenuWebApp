48ff00581d1c34f4bff9e9f67a507d25ecf302eb
nandan <nandan.singhs007@gmail.com>
2023-09-05 13:59:36 +0545

    Adding static files and site design

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -10,3 +10,4 @@ class Item(models.Model):
     item_name = models.CharField(max_length=200)
     item_desc = models.CharField(max_length=200)
     item_price = models.IntegerField()
+    item_image = models.CharField(max_length=500,default="https://cdn-icons-png.flaticon.com/512/1147/1147805.png")
